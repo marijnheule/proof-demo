16dafdd6071e2818bd1e7e8c024e63e6f44e4d20
marijn <marijn@heule.nl>
2018-10-29 15:40:57 -0500

    ACL2-checker

--- drat-step.sh
+++ drat-step.sh
@@ -1,3 +1,12 @@
+echo "CNF:"
+echo ""
+cat cnf/drat-step.cnf
+echo ""
+echo "PROOF:"
+echo ""
+cat proof/drat-step.drat
+echo ""
+
 set -x
 
 ./tools/drat-trim cnf/drat-step.cnf proof/drat-step.drat -f
