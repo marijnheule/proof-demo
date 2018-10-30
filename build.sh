680dbe96a67263e474f5c29cc2c1db2bbd5b0306
marijn <marijn@heule.nl>
2018-10-30 11:25:20 -0500

    lingeling build

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@ gcc tools/drat-trim.c     -O2 -o tools/drat-trim
 gcc tools/compress-lrat.c -O2 -o tools/compress-lrat
 gcc tools/lrat-check.c    -O2 -o tools/lrat-check
 
-cd iglucose/simp; make
+cd iglucose/simp; make; cd ../..
+cd druplig; ./configure.sh; make; cd ..
+cd lingeling-bbc; ./configure.sh; make; cd ..
